f711d46e2fcf6e3744ea63b3f9270702270df428
Iann Lucas <contact.iannlucas@gmail.com>
2025-05-05 17:02:42 -0300

    Aprendi sobre usar scanner

--- ADV(Aula06)/src/App.java
+++ ADV(Aula06)/src/App.java
@@ -1,10 +1,28 @@
-import javax.swing.JOptionPane;
+
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String filme = JOptionPane.showInputDialog(null , "Qual é o seu filme favorito da sua vida?" , "iannzin30" , JOptionPane.QUESTION_MESSAGE);
+       /*
+       String filme = JOptionPane.showInputDialog(null , "Qual é o seu filme favorito da sua vida?" , "iannzin30" , JOptionPane.QUESTION_MESSAGE);
         System.out.println(filme);
+
+        */ 
+
+        String jogo;
+        System.out.println("Qual é o seu jogo do momento ?");
+        Scanner leitor = new Scanner(System.in);
+        leitor.useDelimiter("[\r\n]+");
+
+
+
+        jogo = leitor.next();
+        System.out.println("O jogo do momento é  " + jogo);
+
+        leitor.close(); // fecha o Scanner
+
 
         
     }
 }
+
