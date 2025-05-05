148113e200080898bc84fc9eb51f83734a8e2359
Iann Lucas <contact.iannlucas@gmail.com>
2025-05-05 16:11:39 -0300

    Mudei o nome dos arquivos

---
+++ ADV(Aula06)/src/App.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String filme = JOptionPane.showInputDialog(null , "Qual é o seu filme favorito da sua vida?" , "iannzin30" , JOptionPane.QUESTION_MESSAGE);
+        System.out.println(filme);
+
+        
+    }
+}
