57d5c5dca1ab4d04140190e45090b1995379b790
Iann Lucas <contact.iannlucas@gmail.com>
2025-05-29 18:39:39 -0300

    Revisei os tipos de laços de repetições

---
+++ FIAP(Cáp04)/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a = 0;
+        while(a < 5){
+        System.out.println("O valor de A é " + a);
+        a++;
+        }
+
+        int b = 90;
+        do {
+            System.out.println("O valor de B é " + b);
+            b--;
+        } while (b >= 0);
+
+
+        for(int c = 100; c < 500 ; c++ ){
+            System.out.println("O valor de C é " + c);
+        }
+
+    }
+}
