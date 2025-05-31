03c1a2fb0293596b021258cc755d990c6b6eab12
Iann Lucas <contact.iannlucas@gmail.com>
2025-05-31 19:53:20 -0300

    Aprendi sobre métodos

--- FIAP(Cáp0506)/src/Computador.java
+++ FIAP(Cáp0506)/src/Computador.java
@@ -10,5 +10,19 @@ public class Computador {
         boolean placaDeVideoIntegrada;
 
         Fabricante fabricante;
-         
+
+        public double calcularDesconto(double desconto) {
+                return preco * desconto;
+        }
+
+        boolean adicionarMemoriaRam(int quantidade) {
+                if (quantidade % 2 == 1) return false;
+                memoriaRam += quantidade;
+                preco += quantidade * 150;
+                return true;
+        }
+
+        
 }
+         
+
